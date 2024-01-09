b6e773e5b404f7f9374b708c4e7d216778d93f1e
haris <muhammadharis40781@gmail.com>
2024-01-09 18:19:41 +0500

    first

---
+++ first.js
@@ -0,0 +1,41 @@
+console.log("WELCOME TO PJS");
+console.log("HELLO WORLD");
+
+// DATA TYPES
+firstName = "Haris";
+age = 21;
+x = "null";
+y = undefined;
+isCnic = true;
+
+console.log(typesof(isCnic));
+console.log(typesof(firstName));
+console.log(typesof(age));
+console.table(firstName  , age , x, y , isCnic);
+
+// Two TYPES OF DATA TYPES
+// PRIMITIVE STRING , INT , NUMBER , BOOLEAN ets
+// NON PRIMITIVE ARRAY , OBJECT , FUNCTION ets
+
+// THREE TYPES OF DECLERATION
+// VAR can be redeclared and updated
+// Let can't be redeclared but it can be updated
+// Const can't be redeclared or can't be updated
+                                                                  
+var city = "karachi";
+console.log(city);
+city = "ISLAMABAD";
+var city = "lahore";
+console.log(city);
+
+let myAge = 21;
+console.log(myAge);
+myAge = 23;
+console.log(myAge);
+
+const pie = 3.14;
+console.log(pie);
+pie = 4.25;
+
+
+
