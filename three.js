6c83d3c5153a4cd420828c2c16360ffeb5827ff9
asp <asp@aptech.com>
2024-01-11 18:26:10 +0500

    save

--- three.js
+++ three.js
@@ -16,3 +16,32 @@ console.log(student.age);
 for (let i in student){
     console.log(i , student[i])
 }
+
+let a = 60;
+let b = 56;
+
+console.log("the sum of a+b is : " , a+b);
+console.log("the sub of a-b is : " , a-b);
+console.log("the mul of a*b is : " , a*b);
+console.log("the modulus of a%b is : " , a%b);
+console.log("the div of a/b is : " , a/b);
+
+// Logical operators
+
+// or || ek cond bhi true hoge to apka output true hojayega
+//  And &&
+//  NOT !
+
+//  OR
+console.log("OR GATE");
+console.log(true || true);
+console.log(true || false);
+console.log(true || false);
+console.log(true || false);
+
+//  AND !
+console.log("AND GATE");
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
+console.log(true && true);
