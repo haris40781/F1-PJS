f2c9a1cd722fdde6d267a6159502d7e13a0ca9c7
asp <asp@aptech.com>
2024-01-13 18:00:14 +0500

    save

---
+++ four.js
@@ -0,0 +1,22 @@
+// let age = 20
+// if(age>= 20){
+//     console.log("you can cast your vote")
+// }
+
+// if(age>= 20){
+//     console.log("you can not cast your vote")
+// }
+
+let mode = prompt('Enter your mode')
+1)DARK
+2)LIGHT
+
+if(mode === "DARK"){
+    color = "black";
+}
+if(mode === "LIGHT"){
+    color = "white"
+}
+
+
+console.log(COLOR);
